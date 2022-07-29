d99dfdedfa4552f5b187f9ca4ab9198a4102873b
Tony <tony@gmail.com>
2022-07-29 13:39:10 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,13 +1,18 @@
 package com.sunnyweather.android.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.model.getSky
@@ -20,6 +25,10 @@ class WeatherActivity : AppCompatActivity() {
         ViewModelProvider(this).get(WeatherVIewModel::class.java)
     }
 
+    lateinit var swipeRefresh: SwipeRefreshLayout
+
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
@@ -29,6 +38,8 @@ class WeatherActivity : AppCompatActivity() {
             viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
         if (viewModel.placeName.isEmpty())
             viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+
+        swipeRefresh = findViewById(R.id.swipeRefresh)
         viewModel.weatherLiveData.observe(this, Observer { result ->
             val weather = result.getOrNull()
             if (weather != null)
@@ -37,13 +48,37 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
-        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.setColorSchemeResources(
+            com.google.android.material.R.color.design_default_color_primary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
 
         val decorView = window.decorView
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         window.statusBarColor = Color.TRANSPARENT
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        findViewById<Button>(R.id.navBtn).setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(
+                    drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
     }
 
     private fun showWeatherInfo(weather: Weather) {
@@ -86,5 +121,10 @@ class WeatherActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.ultravioletText).text = lifeIndex.ultraviolet[0].desc
         findViewById<TextView>(R.id.carWashingText).text = lifeIndex.carWashing[0].desc
         findViewById<ScrollView>(R.id.weatherLayout).visibility = View.VISIBLE
+    }
+
+    fun refreshWeather() {
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
     }
 }
